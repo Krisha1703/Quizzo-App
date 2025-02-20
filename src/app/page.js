6ed97a5dab15ae909ea953a83bf87ede624a87a8
Krisha Botadara <krishabotadara@gmail.com>
2025-02-20 23:42:04 +0700

    Homepage an instruction updated

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="w-5/6 mx-auto p-4 my-5">
       <Navbar />
 
-      <div className="md:flex flex-col justify-between space-y-4 md:space-x-4 md:space-y-0">
+      <div className="flex md:flex-row flex-col justify-between space-y-4 md:space-x-4 md:space-y-0">
         <Card title={"General Knowledge"} src={"/Assets/general-knowledge.png"} onClick={router.push("/quiz")}/>
         <Card title={"Science & Nature"} src={"/Assets/science-nature.png"}/>
         <Card title={"Geography"} src={"/Assets/geography.png"}/>
