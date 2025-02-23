acc60c5c0a745ea8fb9ec9d66f8f96966c6f836d
Krisha Botadara <krishabotadara@gmail.com>
2025-02-23 20:44:56 +0700

    resolved deploy error

--- src/components/homepage/card-slider.js
+++ src/components/homepage/card-slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { IconButton } from "@mui/material";
 import Card from "@/components/homepage/quiz-card";
@@ -7,8 +7,14 @@ const CardSlider = ({ cards }) => {
   const [hoveredPrev, setHoveredPrev] = useState(false);
   const [hoveredNext, setHoveredNext] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const isSmallScreen = window.innerWidth < 768;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsSmallScreen(window.innerWidth < 768);
+    }
+  }, []);
+
   const cardsPerPage = isSmallScreen ? 1 : 4;
 
   const displayedCards = cards.slice(currentIndex, currentIndex + cardsPerPage);
@@ -27,7 +33,7 @@ const CardSlider = ({ cards }) => {
 
   return (
     <div className="w-full mx-auto my-5">
-      <div className="flex flex-row  justify-center items-center md:gap-4 m-5">
+      <div className="flex flex-row justify-center items-center md:gap-4 m-5">
         {/* Prev Button */}
         <IconButton
           sx={{
